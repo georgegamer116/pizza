b09773a52d8e3f105fb2ea950df9b2d811d62e98
georgegamer116 <gamergeorge993@gmail.com>
2025-09-08 22:17:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 // hello.js
 
-console.log("Hello, World!");
+// server.js
+
+const http = require('http');
+
+// Create server
+const server = http.createServer((req, res) => {
+  res.statusCode = 200; // OK
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<h1>Hello, World!</h1>');
+});
+
+// Start server
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
